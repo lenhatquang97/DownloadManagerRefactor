0471d2519c1431e6aa3115bbe8f8e7acf274bc00
quangln2 <quangln2@vng.com.vn>
2022-08-25 16:24:52 +0700

    Fix Gitlab Exception

--- app/src/main/java/com/quangln2/downloadmanagerrefactor/data/source/protocol/SocketProtocol.kt
+++ app/src/main/java/com/quangln2/downloadmanagerrefactor/data/source/protocol/SocketProtocol.kt
@@ -117,7 +117,6 @@ class SocketProtocol : ProtocolInterface, Serializable {
             }
             return file
         } catch (e: Exception) {
-            //Exception
             Log.d("FileError", e.toString())
             val initFile = ServiceLocator.initializeStructureDownFile()
             file.fileName = initFile.fileName
