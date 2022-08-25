dda442187e1ac6b2776bc9fdfe212372295ce220
quangln2 <quangln2@vng.com.vn>
2022-08-25 17:17:20 +0700

    Complete Stop and Retry button

--- app/src/main/java/com/quangln2/downloadmanagerrefactor/data/source/protocol/SocketProtocol.kt
+++ app/src/main/java/com/quangln2/downloadmanagerrefactor/data/source/protocol/SocketProtocol.kt
@@ -11,6 +11,7 @@ import com.quangln2.downloadmanagerrefactor.data.model.downloadstatus.DownloadSt
 import com.quangln2.downloadmanagerrefactor.util.UIComponentUtil
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.flow
 import org.json.JSONObject
 import java.io.DataInputStream
@@ -27,12 +28,11 @@ class SocketProtocol : ProtocolInterface, Serializable {
     var socket: Socket? = null
     var inp: DataInputStream? = null
     var out: DataOutputStream? = null
-    val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     constructor(ip: String, port: Int) {
         this.ip = ip
         this.port = port
-        scope.launch { connectToServer(ip, port) }
+        CoroutineScope(Dispatchers.IO).launch { connectToServer(ip, port) }
     }
 
     companion object {
@@ -47,10 +47,8 @@ class SocketProtocol : ProtocolInterface, Serializable {
     private fun sendMessageWithSyntaxCommandContent(mess: String) {
         val command = mess.split("|")[0]
         val content = mess.split("|")[1]
-        scope.launch {
-            out?.write("${createJSONRule(command, content)}\n".toByteArray())
-            out?.flush()
-        }
+        out?.write("${createJSONRule(command, content)}\n".toByteArray())
+        out?.flush()
 
     }
 
@@ -67,7 +65,7 @@ class SocketProtocol : ProtocolInterface, Serializable {
     }
     fun closeConnection() {
         try{
-            scope.launch {
+            CoroutineScope(Dispatchers.IO).launch {
                 out?.write("${createJSONRule("exit", "bye")}\n".toByteArray())
                 out?.flush()
                 socket?.close()
@@ -97,9 +95,7 @@ class SocketProtocol : ProtocolInterface, Serializable {
 
     override fun fetchDownloadInfo(file: StructureDownFile): StructureDownFile {
         try {
-            val scope = CoroutineScope(Dispatchers.IO)
-
-            scope.launch {
+            CoroutineScope(Dispatchers.IO).launch {
                 sendMessageWithSyntaxCommandContent("getFileInfo|${file.fileName}")
                 var result = inp?.readLine()
                 while(result == null){
@@ -125,57 +121,80 @@ class SocketProtocol : ProtocolInterface, Serializable {
         }
     }
 
-    override fun downloadAFile(file: StructureDownFile, context: Context): Flow<StructureDownFile> = flow {
-        val appSpecificExternalDir = File(context.getExternalFilesDir(null), file.chunkNames[0])
-        val fos = FileOutputStream(appSpecificExternalDir.absolutePath, true)
+    override fun downloadAFile(file: StructureDownFile, context: Context): Flow<StructureDownFile> = channelFlow {
+        try{
+            val appSpecificExternalDir = File(context.getExternalFilesDir(null), file.chunkNames[0])
+            val fos = FileOutputStream(appSpecificExternalDir.absolutePath, true)
 
-        val fileNameSegmented = file.downloadLink.split("/")[1]
-        out?.write("${createJSONRule("sendFile", fileNameSegmented)}\n".toByteArray())
-        out?.flush()
+            val fileNameSegmented = file.downloadLink.split("/")[1]
+            out?.write("${createJSONRule("sendFile", fileNameSegmented)}\n".toByteArray())
+            out?.flush()
 
-        var bytesRead = inp?.readBytes()
+            var bytesRead = inp?.readBytes()
 
-        while (bytesRead != null && bytesRead.isNotEmpty()) {
-            file.bytesCopied += bytesRead.size
-            fos.write(bytesRead)
-            emit(file)
-            bytesRead = inp?.readBytes()
+            while (bytesRead != null && bytesRead.isNotEmpty()) {
+                file.bytesCopied += bytesRead.size
+                fos.write(bytesRead)
+                send(file)
+                bytesRead = inp?.readBytes()
+            }
+        } catch(e: Exception){
+            send(file.copy(downloadState = DownloadStatusState.FAILED))
+            deleteTempFiles(file, context)
         }
     }
 
     override fun resumeDownload(file: StructureDownFile, context: Context) {
-        scope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             connectToServer(ip, port)
-            val appSpecificExternalDir = File(context.getExternalFilesDir(null), file.fileName)
-            sendMessageWithSyntaxCommandContent("resume|${file.fileName}?${appSpecificExternalDir.length()}")
+            val appSpecificExternalDir = File(context.getExternalFilesDir(null), file.chunkNames[0])
+            val actualFileName = file.downloadLink.split("/")[1]
+            out?.write("${createJSONRule("resume", "$actualFileName?${appSpecificExternalDir.length()}")}\n".toByteArray())
+            out?.flush()
             file.downloadState = DownloadStatusState.RESUMED
             downloadAFile(file, context)
         }
     }
 
     override fun pauseDownload(file: StructureDownFile) {
-        scope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             socket?.close()
-            sendMessageWithSyntaxCommandContent("pause|${file.fileName}")
+            val actualFileName = file.downloadLink.split("/")[1]
+            sendMessageWithSyntaxCommandContent("pause|$actualFileName")
         }
     }
 
     override fun stopDownload(item: StructureDownFile, context: Context) {
-        scope.launch {
-            socket?.close()
-            sendMessageWithSyntaxCommandContent("stop|${item.fileName}")
-            val file = File(context.getExternalFilesDir(null), item.fileName)
+        item.bytesCopied = 0
+        item.listChunks = item.listChunks.map { it.copy(curr = it.from) }.toMutableList()
+        item.downloadState = DownloadStatusState.FAILED
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val actualFileName = item.downloadLink.split("/")[1]
+            out?.write("${createJSONRule("stop", actualFileName)}\n".toByteArray())
+            out?.flush()
+            val file = File(context.getExternalFilesDir(null), item.chunkNames[0])
             if (file.exists()) {
                 file.delete()
             }
         }
+
+
     }
 
     override fun retryDownload(file: StructureDownFile, context: Context) {
-        scope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             connectToServer(ip, port)
-            sendMessageWithSyntaxCommandContent("retry|${file.fileName}")
-            downloadAFile(file, context)
+            file.downloadState = DownloadStatusState.DOWNLOADING
+            file.bytesCopied = 0
+        }
+    }
+
+    private fun deleteTempFiles(file: StructureDownFile, context: Context) {
+        val appSpecificExternalDir = File(context.getExternalFilesDir(null), file.chunkNames[0])
+        val fos = File(appSpecificExternalDir.absolutePath)
+        if (fos.exists()) {
+            fos.delete()
         }
     }
 
